402d238db93e8d63fe63f4fa122a5c54948af460
Ultimate88x <alejandrogonzalez88x@gmail.com>
2024-05-02 15:04:09 +0200

    Añadidas carpetas y configuración inicial

--- src/main/java/aiss/tiktokminer/TiktokminerApplication.java
+++ src/main/java/aiss/tiktokminer/TiktokminerApplication.java
@@ -2,6 +2,9 @@ package aiss.tiktokminer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TiktokminerApplication {
@@ -10,4 +13,6 @@ public class TiktokminerApplication {
 		SpringApplication.run(TiktokminerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) { return builder.build(); }
 }
