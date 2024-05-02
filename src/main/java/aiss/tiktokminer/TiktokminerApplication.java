6be8bafb9ac7424f8031e837af506d5552172187
Ultimate88x <alejandrogonzalez88x@gmail.com>
2024-05-02 14:45:10 +0200

    Initial commit

---
+++ src/main/java/aiss/tiktokminer/TiktokminerApplication.java
@@ -0,0 +1,13 @@
+package aiss.tiktokminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokminerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aiss/tiktokminer/TiktokminerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.tiktokminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
